6e4ecd24c4dbcb7f96e6f5d6444a8c519946e7a1
artmper <twistertwist1@gmail.com>
2026-07-04 22:11:33 +0300

    add: добавлен RootController и проверка создания куки

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,11 +1,13 @@
 package org.example.hexlet;
 
 import org.example.hexlet.controller.CoursesController;
+import org.example.hexlet.controller.RootController;
 import org.example.hexlet.controller.UsersController;
 
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinJte;
 
+import java.time.LocalDateTime;
 
 public class HelloWorld {
     public static void main(String[] args) {
@@ -14,11 +16,15 @@ public class HelloWorld {
             config.fileRenderer(new JavalinJte());
         });
 
-//        app.before(ctx -> {
-//            ctx.contentType("text/html; charset=UTF-8");
-//        });
+        app.before(ctx -> {
+            ctx.contentType("text/html; charset=UTF-8");
+        });
 
-        app.get("/", ctx -> ctx.render("index.jte"));
+        app.before(ctx -> {
+            System.out.println("Request received at: " + LocalDateTime.now());
+        });
+
+        app.get(NamedRoutes.rootPath(), RootController::index);
 
         app.get(NamedRoutes.usersPath(), UsersController::index);
         app.get(NamedRoutes.buildUserPath(), UsersController::build);
